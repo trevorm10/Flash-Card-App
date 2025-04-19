11c40333f1b8367a541e6862b051a69185e5c233
trevorm10 <trevormoodley10@gmail.com>
2025-04-19 13:21:02 +0200

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+@file:Suppress("UNUSED_EXPRESSION")
+
+import org.gradle.api.initialization.resolve.RepositoriesMode.FAIL_ON_PROJECT_REPOS
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+
+    }
+}
+
+rootProject.name = "myflashcardapp"
+include(":app")
+ 
